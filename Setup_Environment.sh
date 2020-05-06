addd481691918c52f0a9626f7022404dea13c368
Sagar Shah <sagarshah2326@gmail.com>
2020-05-06 12:17:06 +0530

    TCA: Let's create a new bash for command execution

--- Setup_Environment.sh
+++ Setup_Environment.sh
@@ -14,11 +14,3 @@ time apt install ruby pv toilet tsu git wget screenfetch figlet -y
 
 # Install LOLCat via Ruby's package manager
 gem install lolcat
-
-# Remove existing files
-tsu -c 'find . -iname '*Automation.sh*' -exec rm -rf {} \;'
-
-# Fetch the script and setup
-tsudo wget https://raw.githubusercontent.com/TeamTCA/TCA-Automation/master/Automation.sh -O ./Automation.sh
-tsu -c chmod a+x Automation.sh
-tsu -c ./Automation.sh
